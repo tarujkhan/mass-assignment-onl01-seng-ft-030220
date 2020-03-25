859ca1e7ac8caf4624f5f816a2470b1bc01c2523
taroj khan <tarujkhan@yahoo.com>
2020-03-25 19:28:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(attributes)
-    attributes.each |key, value| self.send(("#{key}="), value)}
+  def initialize(name_attributes)
+    name_attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
